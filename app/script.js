a23ff8397786e82a74a92f84e05c310e1549af77
Frank Dai <frank821204@gmail.com>
2015-07-01 10:21:37 +0800

    temp

--- app/script.js
+++ app/script.js
@@ -4,18 +4,15 @@ sp.filter("timestampFormat",function(){
 		return input.slice(0,10)+" "+input.slice(11,19);
 	};
 });
-sp.controller("snaphotoCtrl",["$animate","$scope","$http","$filter","sortData","$timeout",
+sp.controller("snaphotoCtrl",["$animate","$scope","$http","$filter","$timeout",
 function($animate,$scope,$http,$filter,sortData,$timeout){
 	var originalData;
-	var numberPerRows=3;
 	$http.get("/getphotos").success(function(data){
-		originalData=data;
 		$scope.data=data;
-		//$scope.data=sortData(data,numberPerRows);
+		originalData=angular.copy(data);
 	});
 	$scope.arrange=function(recentFirst){
-		$scope.data=$filter("orderBy")(originalData,"taken_time",recentFirst);
-		//$scope.data=sortData(data,numberPerRows);	
+		$scope.data=$filter("orderBy")(originalData,"taken_time",recentFirst);	
 	};
 	$scope.updateTitle=function(id,title) {
 		$scope.toggleEditModel(id);
@@ -23,35 +20,14 @@ function($animate,$scope,$http,$filter,sortData,$timeout){
 	};
 	$scope.updateTitleKeyPress=function(e,id) {
 		if (e.keyCode=="13") {
-			$timeout(function () { e.target.blur(); }, 0, false);
+			$timeout(function () { e.target.blur(); }, 0, false); 
 		}
 	};
 	$scope.search=function(){
 		var key=$scope.keyword;
 		$scope.data=$filter("filter")(originalData,{title:key});
-		//$scope.data=sortData(data,numberPerRows);	
 	};
 	$scope.toggleEditModel=function(id) {
 		id.editModel=!id.editModel;	
 	};
 }]);
-sp.factory("sortData",function(){
-	return function(data,numberPerRow){
-		var results=[];
-		var temp=[];
-		for (var i=0;i<data.length;i++) {
-			if (i%numberPerRow==0 && i!=0) {
-				results.push(temp);
-				temp=[];
-				temp.push(data[i]);
-			}
-			else {
-				temp.push(data[i]);
-			}
-		}
-		if (temp.length) {
-			results.push(temp);
-		}
-		return results;	
-	};
-});
